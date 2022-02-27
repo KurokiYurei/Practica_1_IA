7c35d42f771712445f33c54d3c99ec5fc4e9a459
OscarPazGarre <82536127+OscarPazGarre@users.noreply.github.com>
2022-02-27 19:30:06 +0100

    Fsm

--- Assets/Scripts/FSMs/AntsLvl2FSM.cs
+++ Assets/Scripts/FSMs/AntsLvl2FSM.cs
@@ -3,95 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 using Steerings;
 
-public class AntsLvl2FSM : FiniteStateMachine
+namespace FSM
 {
-    public enum State
+    public class AntsLvl2FSM : FiniteStateMachine
     {
-        GO_TO_CHEESE, LVL1FSM
-    };
-
-    public State currentState = State.GO_TO_CHEESE;
-    private FlockingAround flockingAround;
-    public GameObject cheese;
-    public Transform cheeseSpawner;
-    private bool transportingCheese;
-    private float closeToCheeseSpawnerDistance;
-    private AntFSM antFSM;
-    void Start()
-    {
-        flockingAround = GetComponent<FlockingAround>();
-        closeToCheeseSpawnerDistance = 2.0f;
-        antFSM = GetComponent<AntFSM>();
-    }
-
-    public override void Exit()
-    {
-        base.Exit();
-    }
-
-    public override void ReEnter()
-    {
-        base.ReEnter();
-    }
-
-    void Update()
-    {
-        switch (currentState)
+        public enum State
         {
-            case State.GO_TO_CHEESE:
-                if(SensingUtils.DistanceToTarget(gameObject, cheeseSpawner.gameObject) <= closeToCheeseSpawnerDistance)
-                {
-                    cheese.SetActive(true);
-                    transportingCheese = true;
-                }
-                if(transportingCheese)
-                {
-                    ChangeState(State.LVL1FSM);
-                }
-                break;
+            GO_TO_CHEESE, LVL1FSM
+        };
 
-            case State.LVL1FSM:
-                if (cheese.activeSelf == false)
-                {
-                    ChangeState(State.GO_TO_CHEESE);
-                }
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    private void ChangeState(State newState)
-    {
-        switch (currentState)
+        public State currentState = State.GO_TO_CHEESE;
+        private FlockingAround flockingAround;
+        public GameObject cheese;
+        public Transform cheeseSpawner;
+        private bool transportingCheese;
+        private float closeToCheeseSpawnerDistance;
+        private AntFSM antFSM;
+        void Start()
         {
-            case State.GO_TO_CHEESE:
-                flockingAround.enabled = false;
-                break;
-
-            case State.LVL1FSM:
-                transportingCheese = false;
-                break;
-
-            default:
-                break;
+            flockingAround = GetComponent<FlockingAround>();
+            closeToCheeseSpawnerDistance = 2.0f;
+            antFSM = GetComponent<AntFSM>();
         }
 
-        switch (newState)
+        public override void Exit()
         {
-            case State.GO_TO_CHEESE:
-                flockingAround.attractor = cheeseSpawner.gameObject;
-                flockingAround.seekWeight = 1.0f;
-                flockingAround.enabled = true;
-                break;
+            base.Exit();
+        }
 
-            case State.LVL1FSM:
-                antFSM.ReEnter();
-                break;
+        public override void ReEnter()
+        {
+            base.ReEnter();
+        }
 
-            default:
-                break;
+        void Update()
+        {
+            switch (currentState)
+            {
+                case State.GO_TO_CHEESE:
+                    if (SensingUtils.DistanceToTarget(gameObject, cheeseSpawner.gameObject) <= closeToCheeseSpawnerDistance)
+                    {
+                        cheese.SetActive(true);
+                        transportingCheese = true;
+                    }
+                    if (transportingCheese)
+                    {
+                        ChangeState(State.LVL1FSM);
+                    }
+                    break;
+
+                case State.LVL1FSM:
+                    if (cheese.activeSelf == false)
+                    {
+                        ChangeState(State.GO_TO_CHEESE);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void ChangeState(State newState)
+        {
+            switch (currentState)
+            {
+                case State.GO_TO_CHEESE:
+                    flockingAround.enabled = false;
+                    break;
+
+                case State.LVL1FSM:
+                    transportingCheese = false;
+                    break;
+
+                default:
+                    break;
+            }
+
+            switch (newState)
+            {
+                case State.GO_TO_CHEESE:
+                    flockingAround.attractor = cheeseSpawner.gameObject;
+                    flockingAround.seekWeight = 1.0f;
+                    flockingAround.enabled = true;
+                    break;
+
+                case State.LVL1FSM:
+                    antFSM.ReEnter();
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
