f670d08f49e65894eed137056d630588edaca4a2
OscarPazGarre <82536127+OscarPazGarre@users.noreply.github.com>
2022-02-27 19:36:14 +0100

    Ants

--- Assets/Scripts/FSMs/AntsFSM.cs
+++ Assets/Scripts/FSMs/AntsFSM.cs
@@ -3,111 +3,114 @@ using System.Collections.Generic;
 using UnityEngine;
 using Steerings;
 
-public class AntFSM : FiniteStateMachine
+namespace FSM
 {
-    public enum State
+    public class AntFSM : FiniteStateMachine
     {
-        INITIAL, WANDERING, GO_TO_TARGET
-    };
-
-    public State currentState = State.INITIAL;
-    private FlockingAround flockingAround;
-    public GameObject userControlledTarget;
-    private float closeToFollowTargetDistance;
-    private float closeToTargetDistance;
-
-
-    void Start()
-    {
-        flockingAround = GetComponent<FlockingAround>();
-
-        closeToTargetDistance = 10.0f;
-        closeToFollowTargetDistance = 30.0f;
-    }
-
-    public override void Exit()
-    {
-        flockingAround.enabled = false;
-        base.Exit();
-    }
-
-    public override void ReEnter()
-    {
-        currentState = State.INITIAL;
-        base.ReEnter();
-    }
-
-    void Update()
-    {
-        switch(currentState)
+        public enum State
         {
-            case State.INITIAL:
-                flockingAround.seekWeight = 0.3f;
-                ChangeState(State.WANDERING);
-                break;
+            INITIAL, WANDERING, GO_TO_TARGET
+        };
 
-            case State.WANDERING:
-                if(SensingUtils.DistanceToTarget(gameObject, userControlledTarget) <= closeToFollowTargetDistance)
-                {
-                    ChangeState(State.GO_TO_TARGET);
-                }
-                break;
+        public State currentState = State.INITIAL;
+        private FlockingAround flockingAround;
+        public GameObject userControlledTarget;
+        private float closeToFollowTargetDistance;
+        private float closeToTargetDistance;
 
-            case State.GO_TO_TARGET:
-                if (SensingUtils.DistanceToTarget(gameObject, userControlledTarget) > closeToFollowTargetDistance)
-                {
+
+        void Start()
+        {
+            flockingAround = GetComponent<FlockingAround>();
+
+            closeToTargetDistance = 10.0f;
+            closeToFollowTargetDistance = 30.0f;
+        }
+
+        public override void Exit()
+        {
+            flockingAround.enabled = false;
+            base.Exit();
+        }
+
+        public override void ReEnter()
+        {
+            currentState = State.INITIAL;
+            base.ReEnter();
+        }
+
+        void Update()
+        {
+            switch (currentState)
+            {
+                case State.INITIAL:
                     flockingAround.seekWeight = 0.3f;
                     ChangeState(State.WANDERING);
-                }
-                if (SensingUtils.DistanceToTarget(gameObject, userControlledTarget) <= closeToTargetDistance)
-                {
-                    flockingAround.seekWeight = 0.7f;
-                    ChangeState(State.WANDERING);
-                }
-                break;
+                    break;
 
-            default:
-                break;
-        }
-    }
+                case State.WANDERING:
+                    if (SensingUtils.DistanceToTarget(gameObject, userControlledTarget) <= closeToFollowTargetDistance)
+                    {
+                        ChangeState(State.GO_TO_TARGET);
+                    }
+                    break;
 
-    private void ChangeState(State newState)
-    {
-        switch (currentState)
-        {
-            case State.INITIAL:
-                break;
+                case State.GO_TO_TARGET:
+                    if (SensingUtils.DistanceToTarget(gameObject, userControlledTarget) > closeToFollowTargetDistance)
+                    {
+                        flockingAround.seekWeight = 0.3f;
+                        ChangeState(State.WANDERING);
+                    }
+                    if (SensingUtils.DistanceToTarget(gameObject, userControlledTarget) <= closeToTargetDistance)
+                    {
+                        flockingAround.seekWeight = 0.7f;
+                        ChangeState(State.WANDERING);
+                    }
+                    break;
 
-            case State.WANDERING:
-                flockingAround.enabled = false;
-                break;
-
-            case State.GO_TO_TARGET:
-                flockingAround.enabled = false;
-                break;
-
-            default:
-                break;
+                default:
+                    break;
+            }
         }
 
-        switch (newState)
+        private void ChangeState(State newState)
         {
-            case State.INITIAL:
-                break;
+            switch (currentState)
+            {
+                case State.INITIAL:
+                    break;
 
-            case State.WANDERING:
-                flockingAround.attractor = userControlledTarget;
-                flockingAround.enabled = true;
-                break;
+                case State.WANDERING:
+                    flockingAround.enabled = false;
+                    break;
 
-            case State.GO_TO_TARGET:
-                flockingAround.seekWeight = 1.0f;
-                flockingAround.attractor = userControlledTarget;
-                flockingAround.enabled = true;
-                break;
+                case State.GO_TO_TARGET:
+                    flockingAround.enabled = false;
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
+
+            switch (newState)
+            {
+                case State.INITIAL:
+                    break;
+
+                case State.WANDERING:
+                    flockingAround.attractor = userControlledTarget;
+                    flockingAround.enabled = true;
+                    break;
+
+                case State.GO_TO_TARGET:
+                    flockingAround.seekWeight = 1.0f;
+                    flockingAround.attractor = userControlledTarget;
+                    flockingAround.enabled = true;
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
