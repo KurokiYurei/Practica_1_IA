2438998be5c94bd91a9fc924ac6a16051fd08b87
OscarPazGarre <82536127+OscarPazGarre@users.noreply.github.com>
2022-03-01 12:12:30 +0100

    doorbutton

---
+++ Assets/Scripts/Other Scripts/DoorButtonActivator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButtonActivator : MonoBehaviour
+{
+    public bool openDoor;
+    private bool opened;
+    public GameObject door;
+    private float timer;
+    private float maxTime;
+
+    private void Start()
+    {
+        timer = 0;
+        maxTime = 15;
+        openDoor = false;
+        opened = false;
+    }
+    void Update()
+    {
+        if (openDoor)
+        {
+            timer++;
+            if (!opened)
+            {
+                door.SetActive(true);
+                opened = true;
+            }
+            if(timer >= maxTime)
+            {
+                openDoor = false;
+            }
+        }
+    }
+}
